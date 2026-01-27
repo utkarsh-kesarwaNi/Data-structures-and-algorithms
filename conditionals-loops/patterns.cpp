af6a2e02fff602d135452d0235779c5416479f18
utkarsh-kesarwaNi <utkarshk.web74@gmail.com>
2026-01-27 23:05:59 +0530

    patterns-2

--- conditionals-loops/patterns.cpp
+++ conditionals-loops/patterns.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void rightTriangle(int &n)
+void numberSquare(int &n)
+{
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= n; col++)
+        {
+            cout << col << " ";
+        }
+        cout << '\n';
+    }
+}
+
+void rightTriangle1(int &n)
 {
     for (int row = 1; row <= n; row++)
     {
@@ -13,11 +25,66 @@ void rightTriangle(int &n)
     }
 }
 
+void rightTriangle2(int &n)
+{
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout << row << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void rightTriangle3(int &n)
+{
+    int count = 1;
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << "\n";
+    }
+}
+
+void rightTriangle4(int &n)
+{
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout << row + col - 1 << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void rightTriangle5(int &n)
+{
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            cout << row - col + 1 << " ";
+        }
+        cout << "\n";
+    }
+}
+
 int main()
 {
     int n;
     cout << "Enter number of rows : ";
     cin >> n;
-    rightTriangle(n);
+    numberSquare(n);
+    rightTriangle1(n);
+    rightTriangle2(n);
+    rightTriangle3(n);
+    rightTriangle4(n);
+    rightTriangle5(n);
     return 0;
 }
